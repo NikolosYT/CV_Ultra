042b5712eaf5b7a393c36d4e53402f803a1ffec4
Nikita <chernyaev151008@gmail.com>
2026-06-11 22:26:46 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace CV_Ultra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+        }
+    }
+}
